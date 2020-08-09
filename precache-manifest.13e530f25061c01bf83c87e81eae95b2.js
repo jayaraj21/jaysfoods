2d77edf0e6713db672dd8f96782d9a17878b76a1
jay z <jayaraj.vis3@gmail.com>
2020-08-10 02:35:15 +0530

    pushing to remote

---
+++ precache-manifest.13e530f25061c01bf83c87e81eae95b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4678ae84f737a06ea6a50ac89fb76e7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25fbdfa0b40b6e137aa9",
+    "url": "/static/css/main.1d48229d.chunk.css"
+  },
+  {
+    "revision": "69dcb4b5897cad38e382",
+    "url": "/static/js/2.c096b06f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c096b06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fbdfa0b40b6e137aa9",
+    "url": "/static/js/main.dc17ec7a.chunk.js"
+  },
+  {
+    "revision": "96e250c057b9dde0225b",
+    "url": "/static/js/runtime-main.524375a1.js"
+  }
+]);
